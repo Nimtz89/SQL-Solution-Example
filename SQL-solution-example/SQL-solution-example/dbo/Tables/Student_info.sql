f394e8f92b195a7cf74778e66f793036e8c08a73
Anco van den Berg <anco.vandenberg@winvision.nl>
2023-07-25 16:09:51 +0200

    Project

---
+++ SQL-solution-example/SQL-solution-example/dbo/Tables/Student_info.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Student_info] (
+    [student_id] INT          NOT NULL,
+    [first_name] VARCHAR (50) NULL,
+    [last_name]  VARCHAR (50) NULL,
+    [age]        INT          NULL,
+    PRIMARY KEY CLUSTERED ([student_id] ASC)
+);
+
---
+++ SQL-solution-example/SQL-solution-example/dbo/Tables/courses.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[courses]
+(
+	[Course_id] INT NOT NULL PRIMARY KEY,
+	[Course_name] VARCHAR(100) NULL,
+	[Instructor] VARCHAR(100) NULL,
+) 
