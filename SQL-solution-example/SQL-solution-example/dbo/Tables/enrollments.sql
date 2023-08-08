93d30cc64eee95bda3f2afff8d6a476baea830aa
Anco van den Berg <anco.vandenberg@winvision.nl>
2023-08-08 11:50:21 +0200

    Added constraint

--- SQL-solution-example/SQL-solution-example/dbo/Tables/enrollments.sql
+++ SQL-solution-example/SQL-solution-example/dbo/Tables/enrollments.sql
@@ -1,11 +1,15 @@
 ﻿CREATE TABLE [dbo].[enrollments] (
-    [enrollment_id]   INT      NOT NULL,
-    [student_id]      INT      NULL,
-    [course_id]       INT      NULL,
-    [enrollment_date] DATE     NULL,
-    [grade]           CHAR (2) NULL,
+    [enrollment_id]   INT  NOT NULL,
+    [student_id]      INT  NULL,
+    [course_id]       INT  NULL,
+    [enrollment_date] DATE NULL,
+    [grade]           INT  NULL,
+    [completed]       BIT  NULL,
     PRIMARY KEY CLUSTERED ([enrollment_id] ASC),
+    CONSTRAINT [check_grade_value] CHECK ([grade]>=(0) AND [grade]<=(10)),
     FOREIGN KEY ([course_id]) REFERENCES [dbo].[courses] ([Course_id]),
     FOREIGN KEY ([student_id]) REFERENCES [dbo].[Student_info] ([student_id])
 );
+
+
 
